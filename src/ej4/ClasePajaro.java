63de2228ccb8154dbb0ebf6bb93de264cb25a383
Luciano Cipriani <lucianocipria@hotmail.com>
2021-10-15 19:10:58 -0300

    mi commit Luciano

---
+++ src/ej4/ClasePajaro.java
@@ -0,0 +1,57 @@
+
+package ej4;
+
+public class ClasePajaro {
+    private String color;
+    private String clase;
+    private String alimento;
+    private Integer distancia;
+
+    public ClasePajaro() {
+    }
+
+    public ClasePajaro(String color, String clase, String alimento, Integer distancia) {
+        this.color = color;
+        this.clase = clase;
+        this.alimento = alimento;
+        this.distancia = distancia;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getClase() {
+        return clase;
+    }
+
+    public void setClase(String clase) {
+        this.clase = clase;
+    }
+
+    public String getAlimento() {
+        return alimento;
+    }
+
+    public void setAlimento(String alimento) {
+        this.alimento = alimento;
+    }
+
+    public Integer getdistancia() {
+        return distancia;
+    }
+
+    public void setdistancia(Integer distancia) {
+        this.distancia = distancia;
+    }
+    
+    
+    
+    
+}
+
+
