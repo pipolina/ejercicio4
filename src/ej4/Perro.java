18274c0f9d859cfc7207f833649c321cf0ef50c5
Daniela Busetti <daniela.verobd@gmail.com>
2021-10-15 20:24:21 -0300

    Commit DAni

---
+++ src/ej4/Perro.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej4;
+
+/**
+ *
+ * @author vero2
+ */
+public class Perro {
+    
+    private String color;
+
+    public Perro() {
+    }
+
+    public Perro(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    
+    
+}
